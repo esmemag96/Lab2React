dd5fb9aa5e135f1ba614a43cc8990394328dca27
Eric Parton <partonemp@gmail.com>
2019-09-05 12:42:49 -0500

    Colores

--- src/TableCellComponent.js
+++ src/TableCellComponent.js
@@ -3,8 +3,17 @@ import TableCell from '@material-ui/core/TableCell';
 
 class TableCellComponent extends React.Component {
   render() {
+
+    var colourStrength = this.props.data.replace(/\,/g,'')
+    colourStrength = Number(colourStrength)
+    colourStrength = 255 - (colourStrength/100000)*255;   //Divide the data value by 100,000 then multiply by 255 to get an 8 bit value for the red
+        console.log(colourStrength);
+    if(colourStrength > 255) {colourStrength = 255}   //100,000 is the cap value.  Anything above is pure red
+    if(colourStrength > 0){   //Poor man's NaN
+      var colour = {backgroundColor: "rgb(255, " + colourStrength + "," + colourStrength+ ")"};
+    }
     return (
-        <TableCell> {this.props.data} </TableCell>
+      <TableCell style={colour}> {this.props.data} </TableCell>
     );
   }
 }
